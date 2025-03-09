a401c23af197af7710b11ab1f7bca85bac5aa4ff
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-09 12:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//your JS code here. If required.
 const fs = require("fs");
-fs.readfile('student.csv', 'utf8', function (err,data) {
-if(err){
-	return console.error(err);
-}	
+fs.readFile('student.csv', 'utf8', function (err, data) {
+    if(err){
+        return console.error(err);
+    }   
+    let rows = data.split('\n');
+    rows = rows.map(row => row.split(','));
+    for(let i = 1; i < rows.length; i++){
+        let sum = 0;
+        for (let j = 1; j < rows[i].length; j++) {
+            sum += Number(rows[i][j]);
+        }
+        let avg = sum/ (rows[i].length - 1);
+        console.log(`Average grade for ${rows[i][0]}: ${avg}`);
+    }
 })
-let rows = data.split('\n');
-rows = rows.map(row => row.split(','));
-for(let i = 1; i < rows.length; i++){
-	let sum = 0;
-		for (let j = 1; j < rows[i].length; j++) {
-		sum += Number(rows[i][j]);
-	}
-	let avg = sum/ (rows[i].length - 1);
-	console.log(`Average grade for ${rows[i][0]}: ${avg}`);
-}
